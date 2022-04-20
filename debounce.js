19b8848b1048e7ba3fc2f2d6c4b78fed31d5ba4f
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-20 09:10:32 +0900

    update debounce comment

--- debounce.js
+++ debounce.js
@@ -13,24 +13,24 @@ function debounce(callback, delay, immediate = false) {
   // create standard anynomous function for "this" binding
   return function (...args) {
     // when function is called, it will clear prev timer
-    // which create closure, meaning that delay every call
+    // which creates closure, meaning it will delay every call
     clearTimeout(timerID);
 
     // create var which gets assigned boolean value
     // by checking if timerID is null and immediate flag to be true
     const shouldCallImmediately = timerID == null && immediate;
-    // and in this case we call the function immediately
+    // and if this is the case we call the function immediately
     if (shouldCallImmediately) {
       callback.apply(this, args);
     }
-    // here we are creating the timer
+
+    // and here we are creating the timer
     timerID = setTimeout(() => {
       // once the delay is finished, we call the callback function
       if (!immediate) {
-        // execute it
         callback.apply(this, args);
       }
-      //   set timerID to be null
+      //   and set timerID to be null
       //   so that shouldCallImmediately will know when delay will finish
       timerID = null;
     }, delay);
